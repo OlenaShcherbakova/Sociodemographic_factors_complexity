4c691300fa293105064a747e0ff05d5a91c9220c
Olena Shcherbakova <50408261+OlenaShcherbakova@users.noreply.github.com>
2023-01-03 11:09:51 +0100

    Create double_checking_new_sample.R

---
+++ double_checking_new_sample.R
@@ -0,0 +1,81 @@
+source("set_up_general.R")
+
+#checking whether the languages in final dataframes used in the analysis are the same 
+
+sample <- "reduced"
+
+if(sample == "reduced"){
+  source("set_up_inla.R")
+  
+  metrics_joined <- metrics_joined %>% 
+    filter(!is.na(L1_log10_st)) %>%
+    rename(L1_log_st = L1_log10_st) %>%
+    mutate(L1_copy = L1_log_st) %>%
+    filter(!is.na(L2_prop)) %>%
+    mutate(L2_copy = L2_prop) %>%
+    filter(!is.na(neighboring_languages_st)) %>%
+    filter(!is.na(Official)) %>%
+    filter(!is.na(Education)) %>%
+    filter(!is.na(boundness_st)) %>%
+    filter(!is.na(informativity_st))
+  
+  #dropping tips not in Grambank
+  metrics_joined <- metrics_joined[metrics_joined$Language_ID %in% tree$tip.label, ]
+  
+  metrics_joined %>% write_tsv("data_wrangling/reduced_analysis_sample.tsv")
+
+}
+  
+sample <-  "full"
+if(sample == "full"){
+  source("set_up_inla.R")
+  
+  metrics_joined <- metrics_joined %>% 
+    filter(!is.na(L1_log10_st)) %>%
+    rename(L1_log_st = L1_log10_st) %>%
+    mutate(L1_copy = L1_log_st) %>%
+    filter(!is.na(L2_prop)) %>%
+    mutate(L2_copy = L2_prop) %>%
+    filter(!is.na(neighboring_languages_st)) %>%
+    filter(!is.na(Official)) %>%
+    filter(!is.na(Education)) %>%
+    filter(!is.na(boundness_st)) %>%
+    filter(!is.na(informativity_st))
+  
+  #dropping tips not in Grambank
+  metrics_joined <- metrics_joined[metrics_joined$Language_ID %in% tree$tip.label, ]
+  
+  metrics_joined %>% write_tsv("data_wrangling/full_analysis_sample.tsv")
+}
+
+full <- read_tsv("data_wrangling/full_analysis_sample.tsv")
+reduced <- read_tsv("data_wrangling/reduced_analysis_sample.tsv")
+
+#finding duplicates: 10 duplicates in reduced
+reduced_dup <- reduced[duplicated(reduced$Language_ID),]
+#no duplicates in full
+full_dup <- full[duplicated(full$Language_ID),]
+
+reduced_dup <- reduced %>% 
+  duplicated(Language_ID) %>% 
+  as.data.frame()
+
+ab <- data.frame(a=c(full$Language_ID, rep(NA, times=19)), 
+                 b=reduced$Language_ID,
+                 boundness_full=c(full$boundness_st, rep(NA, times=19)),
+                 boundness_reduced=reduced$boundness_st)
+
+#the discrepancies are found in extra languages present in the reduced sample
+in_full_not_in_reduced <- ab$a[!(ab$a %in% ab$b)]
+in_reduced_not_in_full <-ab$b[!(ab$a %in% ab$b)]
+
+#Are transformed scores the same?
+reduced[!(reduced$Language_ID %in% 
+                        full$Language_ID)]$Language_ID
+
+#checking for suspicious L2 proportions in languages with no or few L1 speakers & not detecting any
+reduced <- reduced %>% 
+  mutate(suspicious = ifelse(L1_log_st < -2 & L2_prop > 0, 1, 0))
+         
+         
+      
