f55b1a996ed0350b2a3ca4fe1bd65d09e686c8af
OlenaShcherbakova <alyona96shcherbakova@gmail.com>
2023-02-08 15:09:13 +0100

    Update plot_maps_main.R

--- plot_maps_main.R
+++ plot_maps_main.R
@@ -1,12 +1,12 @@
 #plotting maps
 source("set_up_inla.R")
 
-metrics_joined <- metrics_joined %>% 
+metrics_joined <- metrics_joined %>%
   filter(!is.na(L1_log10_st)) %>%
   rename(L1_log_st = L1_log10_st) %>%
   mutate(L1_copy = L1_log_st) %>%
   filter(!is.na(L2_prop)) %>%
-  dplyr::mutate(L2_prop  = scale(L2_prop)[,1]) %>%
+  dplyr::mutate(L2_prop  = scale(L2_prop)[, 1]) %>%
   mutate(L2_copy = L2_prop) %>%
   filter(!is.na(neighboring_languages_st)) %>%
   filter(!is.na(Official)) %>%
@@ -17,20 +17,36 @@ metrics_joined <- metrics_joined %>%
 #an overview of area and family coverage
 tab_areas <- as.data.frame(table(metrics_joined$AUTOTYP_area)) %>%
   arrange(desc(Freq))
-  
+
 tab_families <- as.data.frame(table(metrics_joined$Family_ID)) %>%
   arrange(desc(Freq))
 
 
 #dropping tips not in Grambank
-metrics_joined <- metrics_joined[metrics_joined$Language_ID %in% tree$tip.label, ]
+metrics_joined <-
+  metrics_joined[metrics_joined$Language_ID %in% tree$tip.label,]
 tree <- keep.tip(tree, metrics_joined$Language_ID)
 
-x <- assert_that(all(tree$tip.label %in% metrics_joined$Language_ID), msg = "The data and phylogeny taxa do not match")
+x <-
+  assert_that(all(tree$tip.label %in% metrics_joined$Language_ID), msg = "The data and phylogeny taxa do not match")
 
-world <- map_data('world', wrap=c(-25,335), ylim=c(-56,80), margin=T)
+world <-
+  map_data(
+    'world',
+    wrap = c(-25, 335),
+    ylim = c(-56, 80),
+    margin = T
+  )
 
-lakes <- map_data("lakes", wrap=c(-25,335), col="white", border="gray", ylim=c(-55,65), margin=T)
+lakes <-
+  map_data(
+    "lakes",
+    wrap = c(-25, 335),
+    col = "white",
+    border = "gray",
+    ylim = c(-55, 65),
+    margin = T
+  )
 
 #shifting the longlat of the dataframe to match the pacific centered map
 combination <- metrics_joined %>%
@@ -38,87 +54,218 @@ combination <- metrics_joined %>%
 
 #duplicates?
 
-combination %>% 
-  group_by(Language_ID) %>% 
-  mutate(dupe = n()>1) -> combination_dup
-dupes <- combination_dup[combination_dup$dupe == "TRUE",]
+combination %>%
+  group_by(Language_ID) %>%
+  mutate(dupe = n() > 1) -> combination_dup
+dupes <- combination_dup[combination_dup$dupe == "TRUE", ]
 
 
 #Basemap
 basemap <- ggplot(combination) +
-  geom_polygon(data=world, aes(x=long, y=lat, group=group),
-               colour="gray87",
-               fill="gray87", size = 0.5) +
-  geom_polygon(data=lakes, aes(x=long, y=lat, group=group),
-               colour="gray87",
-               fill="white", size = 0.3)  +
+  geom_polygon(
+    data = world,
+    aes(x = long, y = lat, group = group),
+    colour = "gray87",
+    fill = "gray87",
+    size = 0.5
+  ) +
+  geom_polygon(
+    data = lakes,
+    aes(x = long, y = lat, group = group),
+    colour = "gray87",
+    fill = "white",
+    size = 0.3
+  )  +
   theme(
-    panel.grid.major = element_blank(), #all of these lines are just removing default things like grid lines, axises etc
+    panel.grid.major = element_blank(),
+    #all of these lines are just removing default things like grid lines, axises etc
     panel.grid.minor = element_blank(),
-    axis.title.x=element_blank(),
-    axis.title.y=element_blank(),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
     axis.line = element_blank(),
     panel.border = element_blank(),
     panel.background = element_rect(fill = "white"),
     axis.text.x = element_blank(),
     axis.text.y = element_blank(),
-    axis.ticks = element_blank())   +
-  coord_map(projection = "vandergrinten", ylim=c(-56,67))
+    axis.ticks = element_blank()
+  )   +
+  coord_map(projection = "vandergrinten", ylim = c(-56, 67))
 
 #plotting informativity map
-i <- basemap + geom_point(data=combination, aes(x=Longitude, y=Latitude, fill=informativity_st), pch=21, colour = "black", size=1.5, alpha=1, position = position_jitter(width = 1, height = 1, seed=123)) + 
-  scale_fill_viridis_c(option="viridis", direction=-1) +
-  theme(plot.title = element_text(size=20), legend.title = element_text(size=20), legend.text = element_text(size=20), legend.key.size = unit(0.7, units="cm"), legend.position="bottom") + 
+i <-
+  basemap + geom_point(
+    data = combination,
+    aes(x = Longitude, y = Latitude, colour = informativity_st, fill = informativity_st),
+    pch = 21,
+    size = 1.5,
+    alpha = 1,
+    position = position_jitter(
+      width = 1,
+      height = 1,
+      seed = 123
+    )
+  ) +
+  scale_fill_viridis_c(option = "viridis", direction = -1) +
+  scale_color_viridis_c(option = "viridis",
+                        guide = "none",
+                        direction = -1) +
+  theme(
+    plot.title = element_text(size = 20),
+    legend.title = element_text(size = 20),
+    legend.text = element_text(size = 20),
+    legend.key.size = unit(0.7, units = "cm"),
+    legend.position = "bottom"
+  ) +
   guides(fill = guide_colourbar()) +
-  labs(title = "Informativity", fill="score")
+  labs(title = "Informativity", fill = "score")
 
-ggsave(file="output/map_informativity.svg", plot=i, width=10, height=9)
+ggsave(
+  file = "output/map_informativity.svg",
+  plot = i,
+  width = 10,
+  height = 9, 
+  dpi=300
+)
 
 
-#plotting boudnness map
-b <- basemap + geom_point(data=combination, aes(x=Longitude, y=Latitude, fill=boundness_st), pch=21, colour = "black", size=1.5, alpha=1, position = position_jitter(width = 1, height = 1, seed=123)) + 
-  scale_fill_viridis_c(option="magma", direction=-1) + 
-  theme(plot.title = element_text(size=20), legend.title = element_text(size=20), legend.text = element_text(size=20), legend.key.size = unit(0.7, units="cm"), legend.position="bottom") + 
+#plotting boundness map
+b <-
+  basemap + geom_point(
+    data = combination,
+    aes(x = Longitude, y = Latitude, colour = boundness_st, fill = boundness_st),
+    pch = 21,
+    size = 1.5,
+    alpha = 1,
+    position = position_jitter(
+      width = 1,
+      height = 1,
+      seed = 123
+    )
+  ) +
+  scale_fill_viridis_c(option = "magma", direction = -1) +
+  scale_color_viridis_c(option = "magma",
+                        guide = "none",
+                        direction = -1) +
+  theme(
+    plot.title = element_text(size = 20),
+    legend.title = element_text(size = 20),
+    legend.text = element_text(size = 20),
+    legend.key.size = unit(0.7, units = "cm"),
+    legend.position = "bottom"
+  ) +
   guides(fill = guide_colourbar()) +
-  labs(title = "Boundness", fill="score") 
+  labs(title = "Boundness", fill = "score")
 
-ggsave(file="output/map_boundness.svg", plot=b, width=10, height=9)
+ggsave(
+  file = "output/map_boundness.svg",
+  plot = b,
+  width = 10,
+  height = 9, 
+  dpi=300
+)
 
 
 
-i <- basemap + geom_point(data=combination, aes(x=Longitude, y=Latitude, fill=informativity_st, color=informativity_st), pch=21, size=1.5, alpha=1, position = position_jitter(width = 1, height = 1, seed=123)) + 
-  scale_fill_viridis_c(option="viridis", direction=-1) +
-  scale_color_viridis_c(option="viridis", direction=-1) +
-  theme(text = element_text(size = 20), legend.text = element_text(size = 20), legend.key.size = unit(0.8, units="cm"), legend.position="bottom") + 
-  guides(fill = guide_colourbar(), color="none") +
-  labs(fill="informativity")
+i <-
+  basemap + geom_point(
+    data = combination,
+    aes(
+      x = Longitude,
+      y = Latitude,
+      fill = informativity_st,
+      color = informativity_st
+    ),
+    pch = 21,
+    size = 1.5,
+    alpha = 1,
+    position = position_jitter(
+      width = 1,
+      height = 1,
+      seed = 123
+    )
+  ) +
+  scale_fill_viridis_c(option = "viridis", direction = -1) +
+  scale_color_viridis_c(option = "viridis", direction = -1) +
+  theme(
+    text = element_text(size = 20),
+    legend.text = element_text(size = 20),
+    legend.key.size = unit(0.8, units = "cm"),
+    legend.position = "bottom"
+  ) +
+  guides(fill = guide_colourbar(), color = "none") +
+  labs(fill = "informativity")
 
-b <- basemap + geom_point(data=combination, aes(x=Longitude, y=Latitude, fill=boundness_st, color=boundness_st), pch=21, size=1.5, alpha=1, position = position_jitter(width = 1, height = 1, seed=123)) + 
-  scale_fill_viridis_c(option="magma", direction=-1) + 
-  scale_color_viridis_c(option="magma", direction=-1) + 
-  theme(text = element_text(size = 20), legend.text = element_text(size = 20), legend.key.size = unit(0.8, units="cm"), legend.position="bottom") + 
-  guides(fill = guide_colourbar(), color="none") +
-  labs(fill="fusion") 
+b <-
+  basemap + geom_point(
+    data = combination,
+    aes(
+      x = Longitude,
+      y = Latitude,
+      fill = boundness_st,
+      color = boundness_st
+    ),
+    pch = 21,
+    size = 1.5,
+    alpha = 1,
+    position = position_jitter(
+      width = 1,
+      height = 1,
+      seed = 123
+    )
+  ) +
+  scale_fill_viridis_c(option = "magma", direction = -1) +
+  scale_color_viridis_c(option = "magma", direction = -1) +
+  theme(
+    text = element_text(size = 20),
+    legend.text = element_text(size = 20),
+    legend.key.size = unit(0.8, units = "cm"),
+    legend.position = "bottom"
+  ) +
+  guides(fill = guide_colourbar(), color = "none") +
+  labs(fill = "fusion")
 
-two_maps <- b/i 
-ggsave(file="output/maps_vertical.svg", plot=two_maps, width=7, height=10)
+two_maps <- b | i
+ggsave(
+  file = "output/maps.svg",
+  plot = two_maps,
+  width = 10,
+  height = 5, 
+  dpi=300
+)
 
-two_maps <- b|i 
-ggsave(file="output/maps.svg", plot=two_maps, width=10, height=5)
+ggsave(
+  file = "output/maps.pdf",
+  plot = two_maps,
+  width = 10,
+  height = 5, 
+  dpi=300
+)
 
 
 #histograms
-hist_b <- ggplot(metrics_joined, aes(x=boundness_st)) +
-  geom_histogram(color="black", aes(fill=..x..)) +
-  scale_fill_viridis_c(option="magma", direction=-1) +  
-  labs(x = "fusion", fill="score") +
-  theme_classic(base_size = 22) 
+hist_b <- ggplot(metrics_joined, aes(x = boundness_st)) +
+  geom_histogram(color = "black", aes(fill = ..x..)) +
+  scale_fill_viridis_c(option = "magma", direction = -1) +
+  labs(x = "fusion", fill = "score") +
+  theme_classic(base_size = 22)
 
-hist_i <- ggplot(metrics_joined, aes(x=informativity_st)) +
-  geom_histogram(color="black", aes(fill=..x..)) +
-  scale_fill_viridis_c(option="viridis", direction=-1) + 
-  labs(x = "informativity", fill="score") +
-  theme_classic(base_size = 22) 
+hist_i <- ggplot(metrics_joined, aes(x = informativity_st)) +
+  geom_histogram(color = "black", aes(fill = ..x..)) +
+  scale_fill_viridis_c(option = "viridis", direction = -1) +
+  labs(x = "informativity", fill = "score") +
+  theme_classic(base_size = 22)
 
-ggsave(file="output/hist_boundness.svg", plot=hist_b, width=7, height=5)
-ggsave(file="output/hist_informativity.svg", plot=hist_i, width=7, height=5)
+ggsave(
+  file = "output/hist_boundness.svg",
+  plot = hist_b,
+  width = 7,
+  height = 5, 
+  dpi=300
+)
+ggsave(
+  file = "output/hist_informativity.svg",
+  plot = hist_i,
+  width = 7,
+  height = 5, 
+  dpi=300
+)
