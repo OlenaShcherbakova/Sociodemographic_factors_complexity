acd8a3f2bdb775251b259ad703ba7d7214629c4e
OlenaShcherbakova <alyona96shcherbakova@gmail.com>
2023-02-15 06:22:14 +0100

    Update generating_GB_input_file.R

--- generating_GB_input_file.R
+++ generating_GB_input_file.R
@@ -1,41 +1,41 @@
 source("requirements.R")
 
-if(!dir.exists("./grambank-analysed/R_grambank/output")){
-  dir.create("./grambank-analysed/R_grambank/output")}
+if (!dir.exists("./grambank-analysed/R_grambank/output")) {
+  dir.create("./grambank-analysed/R_grambank/output")
+}
 
-#creating a full Grambank file: first, within the submodule itself, and next placing it in the data folder within the repository 
+#creating a full Grambank file: first, within the submodule itself, and next placing it in the data folder within the repository
 
 setwd("grambank-analysed/R_grambank")
 
-if(!(file.exists("./../../data/GB_wide/GB_wide_strict.tsv"))){
+if (!(file.exists("./../../data/GB_wide/GB_wide_strict.tsv"))) {
   cat("Generating GB_wide_strict.\n")
   
   source("make_wide.R")
   
-  read_tsv("output/GB_wide/GB_wide_strict.tsv", show_col_types = F) %>% 
-        write_tsv(file="../../data/GB_wide/GB_wide_strict.tsv")
+  read_tsv("output/GB_wide/GB_wide_strict.tsv", show_col_types = F) %>%
+    write_tsv(file = "../../data/GB_wide/GB_wide_strict.tsv")
 }
 
-#extracting glottolog: first, within the submodule itself, and next placing it in the data folder within the repository 
+#extracting glottolog: first, within the submodule itself, and next placing it in the data folder within the repository
 
-if(!(file.exists("../../../data_wrangling/glottolog_cldf_wide_df.tsv"))){
+if (!(file.exists("../../../data_wrangling/glottolog_cldf_wide_df.tsv"))) {
   cat("Generating glottolog table.\n")
   
   source("make_glottolog-cldf_table.R")
   
-  read_tsv("output/non_GB_datasets/glottolog-cldf_wide_df.tsv", show_col_types = F) %>% 
-    write_tsv(file="./../../data_wrangling/glottolog_cldf_wide_df.tsv") #taking the final dataframe cldf_wide_df from the previous script and saving it in our datafolder
+  read_tsv("output/non_GB_datasets/glottolog-cldf_wide_df.tsv",
+           show_col_types = F) %>%
+    write_tsv(file = "./../../data_wrangling/glottolog_cldf_wide_df.tsv") #taking the final dataframe cldf_wide_df from the previous script and saving it in our datafolder
 }
 
-if(!(file.exists("./../data/GB_wide/parameters.csv"))){
+if (!(file.exists("./../data/GB_wide/parameters.csv"))) {
   cat("Generating parameters table.\n")
   
-read_csv("../grambank/cldf/parameters.csv",show_col_types = F) %>%
-    dplyr::select(ID, Name, Description, boundness, informativity) %>% 
-      write.csv(file="../../data/GB_wide/parameters.csv")
+  read_csv("../grambank/cldf/parameters.csv", show_col_types = F) %>%
+    dplyr::select(ID, Name, Description, boundness, informativity) %>%
+    write.csv(file = "../../data/GB_wide/parameters.csv")
 }
 
 
 setwd("../../")
-
-
