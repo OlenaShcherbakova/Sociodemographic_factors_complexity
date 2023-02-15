325b47b39e7ad942f70fbde237a9522baf7b3878
OlenaShcherbakova <alyona96shcherbakova@gmail.com>
2023-02-15 06:25:34 +0100

    changes to informativity

--- creating_informativity_score.R
+++ creating_informativity_score.R
@@ -9,37 +9,44 @@ if (!dir.exists(OUTPUTDIR2)) { dir.create(OUTPUTDIR2) }
 
 #if (!file.exists(here(OUTPUTDIR2, "informativity_score.tsv"))) { 
 
-GB_wide <- read_tsv(file.path("data", "GB_wide", "GB_wide_strict.tsv"), show_col_types = F)	%>% 
-  filter(na_prop <= 0.25 ) %>% 
+GB_wide <-
+  read_tsv(file.path("data", "GB_wide", "GB_wide_strict.tsv"),
+           show_col_types = F)	%>%
+  filter(na_prop <= 0.25) %>%
   dplyr::select(-na_prop)
 
 #read in sheet with scores for whether a feature denotes informativity
-GB_informativity_points <- read_csv(file.path("data", "GB_wide", "parameters.csv"), show_col_types = F) %>%
+GB_informativity_points <-
+  read_csv(file.path("data", "GB_wide", "parameters.csv"),
+           show_col_types = F) %>%
   dplyr::select(Parameter_ID = ID, informativity) %>%
-  mutate(informativity = replace(informativity, Parameter_ID == "GB177", "argumentanimacy")) %>% #manually adding another parameter: assigning the parameter of GB177 ("Can the verb carry a marker of animacy of argument, unrelated to any gender/noun class of the argument visible in the NP domain?") feature to be informative 
-filter(!is.na(informativity))
+  mutate(informativity = replace(informativity, Parameter_ID == "GB177", "argumentanimacy")) %>% #manually adding another parameter: assigning the parameter of GB177 ("Can the verb carry a marker of animacy of argument, unrelated to any gender/noun class of the argument visible in the NP domain?") feature to be informative
+  filter(!is.na(informativity))
 
-GB_long_for_calc <- GB_wide  %>% 
-  reshape2::melt(id.vars = "Language_ID") %>% 
+GB_long_for_calc <- GB_wide  %>%
+  reshape2::melt(id.vars = "Language_ID") %>%
   rename(Parameter_ID = variable) %>%
   inner_join(GB_informativity_points , by = "Parameter_ID")
 
 ##informativity score
-lg_df_informativity_score <- GB_long_for_calc %>% 
-  mutate(value = if_else(Parameter_ID == "GB140", abs(value-1), value)) %>% # reversing GB140 because 0 is the informative state
+lg_df_informativity_score <- GB_long_for_calc %>%
+  mutate(value = if_else(Parameter_ID == "GB140", abs(value - 1), value)) %>% # reversing GB140 because 0 is the informative state
   group_by(Language_ID, informativity) %>% #grouping per language and per informativity category
-  summarise(sum_informativity = sum(value, na.rm = T), #for each informativity cateogry for each langauge, how many are answered 1 ("yes")
+  summarise(sum_informativity = sum(value, na.rm = T),
+            #for each informativity cateogry for each langauge, how many are answered 1 ("yes")
             sum_na = sum(is.na(value))) %>% #how many of the values per informativity category are missing
-  mutate(sum_informativity = ifelse(sum_na >= 1 & sum_informativity == 0, NA, sum_informativity)) %>% #if there is at least one NA and the sum of values for the entire category is 0, the informativity score should be NA because there could be a 1 hiding under the NA value
-  mutate(informativity_score = ifelse(sum_informativity >= 1, 1, sum_informativity)) %>% 
-  ungroup() %>% 
-  group_by(Language_ID) %>% 
-  summarise(`Informativity`= mean(informativity_score, na.rm = T)) %>% 
-  dplyr::select(Language_ID, `Informativity`) 
+  mutate(sum_informativity = ifelse(sum_na >= 1 &
+                                      sum_informativity == 0, NA, sum_informativity)) %>% #if there is at least one NA and the sum of values for the entire category is 0, the informativity score should be NA because there could be a 1 hiding under the NA value
+  mutate(informativity_score = ifelse(sum_informativity >= 1, 1, sum_informativity)) %>%
+  ungroup() %>%
+  group_by(Language_ID) %>%
+  summarise(`Informativity` = mean(informativity_score, na.rm = T)) %>%
+  dplyr::select(Language_ID, `Informativity`)
 
 informativity_st = scale(lg_df_informativity_score$Informativity)
-lg_df_informativity_score <- cbind(lg_df_informativity_score, informativity_st)
+lg_df_informativity_score <-
+  cbind(lg_df_informativity_score, informativity_st)
 
-lg_df_informativity_score  %>% 		
-  write_tsv(here(OUTPUTDIR2, "informativity_score.tsv"))	
+lg_df_informativity_score  %>%
+  write_tsv(here(OUTPUTDIR2, "informativity_score.tsv"))
 #}
