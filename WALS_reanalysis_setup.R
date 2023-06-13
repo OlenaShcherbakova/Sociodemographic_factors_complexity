31d18acb334ef2258942b8ef02d45d86536ec131
OlenaShcherbakova <alyona96shcherbakova@gmail.com>
2023-06-13 08:14:14 +0200

    Update WALS_reanalysis_setup.R

--- WALS_reanalysis_setup.R
+++ WALS_reanalysis_setup.R
@@ -1,9 +1,8 @@
-source("requirements.R")
+source("install_and_load_INLA.R")
 
 #parameters
 kappa = 1 
 phi_1 = c(1, 1.25) # "Local" version: (sigma, phi) First value is not used
-phi_2 = c(1, 17) # "Regional" version: (sigma, phi) First value is not used
 
 WALS <- read_csv("data/complexity_data_WALS.csv") %>% 
   dplyr::select("Name"=lang, roundComp, logpop2, "ISO_639"=silCode) %>% 
@@ -15,21 +14,27 @@ max_val <- max(WALS$roundComp)
 # Perform the rescaling
 WALS$roundComp <- (WALS$roundComp - min_val) / (max_val - min_val)
 
-pop_file_fn <- "data/Table_of_Languages.tab"
-L1 <- read_tsv(pop_file_fn, show_col_types = F) %>% dplyr::select(ISO_639, L1_Users)
+pop_file_fn <- "data_wrangling/ethnologue_pop_SM_morph_compl_reanalysis.tsv"
+L1 <-
+  read_tsv(pop_file_fn, show_col_types = F) %>% dplyr::select(ISO_639, L1_log10_scaled)
 
 WALS_df <- WALS %>%
   inner_join(L1,
-    by = c("ISO_639")
-  ) %>%  #discarding languages with no or low numbers of L1 speakers and L2 speakers resulting primarily from language revival efforts
-  filter(!is.na(L1_Users)) %>% 
-  dplyr::mutate(L1_Users = log10(L1_Users+1)) %>% 
-  dplyr::mutate(L1_Users_scaled = scale(L1_Users)[,1])
+             by = c("ISO_639")) 
 
-formula <- as.formula(paste("roundComp ~", "L1_Users_scaled"))
-result <- inla(formula, family = "gaussian", 
-                         #control.family = list(hyper = pcprior_hyper), 
+formula <- as.formula(paste("roundComp ~", "L1_log10_scaled"))
+result <- inla(formula, family = "gaussian",  
                          data = WALS_df, control.compute = list(waic = TRUE)) 
 summary(result)
 
-#mean estimate of L1_Users: -0.021 with credible intervals not crossing zero (-0.035 - -0.008)
+save(result, file = "output_models/models_WALS_uncontrolled.RData")
+
+social_effects_uncontrolled <- c("morphological complexity ~ L1",
+  round(c(
+    result$summary.fixed[2,]$`0.025quant`,
+    result$summary.fixed[2,]$`0.5quant`,
+    result$summary.fixed[2,]$`0.975quant`, nrow(WALS_df)), 2), "default (~10%)")
+
+save(social_effects_uncontrolled, file = "output_models/social_effects_uncontrolled.RData")
+
+
