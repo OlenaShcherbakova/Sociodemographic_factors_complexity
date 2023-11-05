9108181da79636a7689c1707c5c8fa452ac0a652
OlenaShcherbakova <alyona96shcherbakova@gmail.com>
2023-11-05 19:35:22 +0100

    Update make_ethnologue_SM_and_merging_tables.R

--- make_ethnologue_SM_and_merging_tables.R
+++ make_ethnologue_SM_and_merging_tables.R
@@ -12,46 +12,101 @@ glottolog_df <- read_tsv("data_wrangling/glottolog_cldf_wide_df.tsv", show_col_t
 GB <- read_tsv("data/GB_wide/GB_wide_strict.tsv", show_col_types = F) %>% 
   dplyr::select(Glottocode = "Language_ID")
 
-colnames(data_ethnologue)
+#colnames(data_ethnologue)
 
-#this script needs the Table_of_languages.tab file to exists, which is only available to people with an SIL lisence
+#this script needs the Table_of_LICs.tab file to exists, which is only available to people with an SIL lisence
 data_ethnologue <- read_tsv("data/Table_of_LICs.tab", show_col_types = F) %>%
+  filter(!is.na(`EGIDS`)) %>%
+  dplyr::mutate(Vehicularity = ifelse(EGIDS == "1" |
+                                        EGIDS == "2" |
+                                        EGIDS == "3", "1", "0")) 
+
+#this dataset will serve as the basis for the reanalysis on the large sample
+data_ethnologue_reanalysis <- data_ethnologue %>%
   filter(!is.na(`All_Users`)) %>% #remove rows with missing data for pop of all users
   filter(!is.na(`L1_Users`)) %>% 
-  filter(!is.na(`L2_Users`)) %>% 
-  filter(`L2_Users` > 0) %>% 
   left_join(glottolog_df, by = "ISO_639" ) %>% 
   dplyr::select(-Glottocode) %>% #removing old Glottocode column
   rename(Glottocode = Language_level_ID) %>% 
   group_by(Glottocode) %>% 
   summarise(All_Users = sum(All_Users, na.rm = T),
             L1_Users = sum(L1_Users, na.rm = T), 
-            L2_Users = sum(L2_Users, na.rm = T), 
-            ISO_639 = paste0(ISO_639, collapse = "; "))
-            
-#do some the subsettting to GB and log10 and L2 prop
-data_ethnologue <- data_ethnologue %>% 
+            ISO_639 = paste0(ISO_639, collapse = "; "),
+            Vehicularity = max(as.numeric(Vehicularity))) %>% 
+  ungroup() %>% 
   inner_join(GB, by = "Glottocode" ) %>% 
-    dplyr::mutate(L2_prop = L2_Users/ All_Users, 
+  dplyr::mutate(L1_log10 = log10(L1_Users+1),
+                All_Users_log10 = log10(All_Users+1)) %>% #adding a 1 for cases where pop is 0
+  dplyr::select(Glottocode, ISO_639, L1_log10, L1_Users, All_Users_log10, All_Users, Vehicularity)
+
+#this dataset will serve as the basis for the reanalysis on the small sample with available L2           
+data_ethnologue_reanalysis_L2 <- data_ethnologue %>%
+  filter(!is.na(`All_Users`)) %>% #remove rows with missing data for pop of all users
+  filter(!is.na(`L1_Users`)) %>% 
+  filter(!is.na(`L2_Users`)) %>% 
+  filter(`L2_Users` >= 0) %>% 
+  left_join(glottolog_df, by = "ISO_639" ) %>% 
+  dplyr::select(-Glottocode) %>% #removing old Glottocode column
+  rename(Glottocode = Language_level_ID) %>% 
+  group_by(Glottocode) %>% 
+  summarise(All_Users = sum(All_Users),
+            L1_Users = sum(L1_Users), 
+            L2_Users = sum(L2_Users), 
+            ISO_639 = paste0(ISO_639, collapse = "; "),
+            Vehicularity = max(as.numeric(Vehicularity))) %>% 
+  ungroup() %>% 
+  inner_join(GB, by = "Glottocode" ) %>% 
+  dplyr::mutate(L2_prop = L2_Users/ All_Users, 
                 #calculating the proportion of L2 users out of the entire population
                 L1_log10 = log10(L1_Users+1),
                 All_Users_log10 = log10(All_Users+1)) %>% #adding a 1 for cases where pop is 0
   mutate(L2_prop = ifelse(All_Users == 0, 0, L2_prop)) %>% #if All users is 0, L2_prop would be NA if we didn't do this (can't divide by 0). It should be 0
-  dplyr::select(Glottocode, ISO_639, L1_log10, L2_prop, L1_Users, All_Users_log10, All_Users)
+  dplyr::select(Glottocode, ISO_639, L1_log10, L2_prop, L1_Users, All_Users_log10, All_Users, Vehicularity)
+
+
+
+#do some the subsettting to GB and log10 and L2 prop
+# data_ethnologue <- data_ethnologue %>% 
+#   inner_join(GB, by = "Glottocode" ) %>% 
+#     dplyr::mutate(L2_prop = L2_Users/ All_Users, 
+#                 #calculating the proportion of L2 users out of the entire population
+#                 L1_log10 = log10(L1_Users+1),
+#                 All_Users_log10 = log10(All_Users+1)) %>% #adding a 1 for cases where pop is 0
+#   mutate(L2_prop = ifelse(All_Users == 0, 0, L2_prop)) %>% #if All users is 0, L2_prop would be NA if we didn't do this (can't divide by 0). It should be 0
+#   dplyr::select(Glottocode, ISO_639, L1_log10, L2_prop, L1_Users, All_Users_log10, All_Users)
 
 #do the scaling
-data_ethnologue$L1_scaled <- scale(data_ethnologue$L1_Users)[,1]
-data_ethnologue$L1_log10_scaled <- scale(data_ethnologue$L1_log10)[,1]
+data_ethnologue_reanalysis$L1_scaled <- scale(data_ethnologue_reanalysis$L1_Users)[,1]
+data_ethnologue_reanalysis_L2$L1_scaled <- scale(data_ethnologue_reanalysis_L2$L1_Users)[,1]
 
-data_ethnologue$All_Users_scaled <- scale(data_ethnologue$All_Users)[,1]
-data_ethnologue$All_Users_log10_scaled <- scale(data_ethnologue$All_Users_log10)[,1]
+data_ethnologue_reanalysis$L1_log10_scaled <- scale(data_ethnologue_reanalysis$L1_log10)[,1]
+data_ethnologue_reanalysis_L2$L1_log10_scaled <- scale(data_ethnologue_reanalysis_L2$L1_log10)[,1]
+
+data_ethnologue_reanalysis$All_Users_scaled <- scale(data_ethnologue_reanalysis$All_Users)[,1]
+data_ethnologue_reanalysis_L2$All_Users_scaled <- scale(data_ethnologue_reanalysis_L2$All_Users)[,1]
+
+data_ethnologue_reanalysis$All_Users_log10_scaled <- scale(data_ethnologue_reanalysis$All_Users_log10)[,1]
+data_ethnologue_reanalysis_L2$All_Users_log10_scaled <- scale(data_ethnologue_reanalysis_L2$All_Users_log10)[,1]
+
 
 #write to file: Ethnologue data for supplementary materials and merging into "reduced" version of the final dataset with social variables (excluding L1_log10)
-data_ethnologue %>% 
-  dplyr::select(ISO_639, Language_ID=Glottocode, L2_prop, L1_scaled, L1_log10_scaled, All_Users_scaled, All_Users_log10_scaled) %>% 
+#large sample
+data_ethnologue_reanalysis %>% 
+  dplyr::select(ISO_639, Language_ID=Glottocode, L1_scaled, L1_log10_st=L1_log10_scaled, Vehicularity, All_Users_scaled, All_Users_log10_scaled) %>% 
   write_tsv("data_wrangling/ethnologue_pop_SM.tsv")
 
+#small L2 sample
+data_ethnologue_reanalysis_L2 %>% 
+  dplyr::select(ISO_639, Language_ID=Glottocode, L2_prop, L1_scaled, L1_log10_st=L1_log10_scaled, Vehicularity, All_Users_scaled, All_Users_log10_scaled) %>% 
+  write_tsv("data_wrangling/ethnologue_pop_L2_SM.tsv")
+
 #write to file: Ethnologue data for merging into "full" version of the final dataset with social variables (including L1_log10) - won't be available to public
-data_ethnologue %>% 
-  dplyr::select(ISO_639, Language_ID=Glottocode, L2_prop, L1_st = L1_scaled, L1_log10_st=L1_log10_scaled, L1_log10 ) %>% 
+#large sample
+data_ethnologue_reanalysis %>% 
+  dplyr::select(ISO_639, Language_ID=Glottocode, L1_st = L1_scaled, L1_log10_st=L1_log10_scaled, L1_log10, Vehicularity ) %>% 
   write_tsv("data_wrangling/ethnologue_pop_full.tsv")
+
+#small L2 sample
+data_ethnologue_reanalysis_L2 %>% 
+  dplyr::select(ISO_639, Language_ID=Glottocode, L2_prop, L1_st = L1_scaled, L1_log10_st=L1_log10_scaled, L1_log10, Vehicularity ) %>% 
+  write_tsv("data_wrangling/ethnologue_pop_L2_full.tsv")
